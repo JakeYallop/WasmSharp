0ea64c8bb1a622bd27812cf1cfe81a21742a41ac
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2023-11-01 22:42:23 +0000

    Tweak styling

--- playground/src/index.css.ts
+++ playground/src/index.css.ts
@@ -5,6 +5,8 @@ globalStyle("#root", {
   backgroundColor: palette.background.main,
   height: "100%",
   width: "100%",
+  display: "flex",
+  flexDirection: "column",
 });
 
 globalStyle("html, body", {
