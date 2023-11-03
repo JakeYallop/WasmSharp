4d28f5be862421e713f8c8d4787443efd0b8cd1d
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2023-11-03 23:32:29 +0000

    Automate some publish steps

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -3,7 +3,11 @@ import solidPlugin from "vite-plugin-solid";
 import ignoreDynamicImports from "vite-plugin-ignore-dynamic-imports";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import inspect from "vite-plugin-inspect";
+import { ResolvedConfig, ViteDevServer, normalizePath, resolvePackageData, resolvePackageEntry } from "vite";
+import path from "path";
+import fs from "fs";
 
+let config: ResolvedConfig;
 export default defineConfig(({ mode }) => {
   return {
     plugins: [
@@ -17,6 +21,55 @@ export default defineConfig(({ mode }) => {
       vanillaExtractPlugin({
         identifiers: mode === "devlopment" ? "debug" : "short",
       }),
+      {
+        configResolved(resolved) {
+          config = resolved;
+        },
+        generateBundle(output) {
+          const data = resolvePackageData("@wasmsharp/core", output.dir!, true);
+          if (!data) {
+            return;
+          }
+          const files = fs.readdirSync(data!.dir, { withFileTypes: true, recursive: true }).filter((x) => {
+            const ext = path.extname(x.name);
+            if (
+              ext === ".ts" ||
+              (ext === ".js" && x.name !== "dotnet.native.js" && x.name !== "dotnet.runtime.js") ||
+              ext === ".pdb" ||
+              ext === ".html" ||
+              ext === ".symbols" ||
+              x.isDirectory()
+            ) {
+              return false;
+            }
+
+            return true;
+          });
+
+          for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const normalizedPath = normalizePath(file.path);
+            const wasmSharpSplitString = "@wasmsharp/core";
+            const parts = normalizedPath.split(wasmSharpSplitString);
+            let finalPath: string;
+            if (parts[1] === undefined) {
+              finalPath = "";
+            } else {
+              finalPath = parts[1].substring(1, parts[1].length);
+            }
+
+            const fileName = path.join(finalPath, file.name);
+            const buffer = fs.readFileSync(path.join(file.path, file.name));
+            const source = new Uint8Array(buffer.buffer);
+            this.emitFile({
+              type: "asset",
+              needsCodeReference: false,
+              fileName: fileName,
+              source: source,
+            });
+          }
+        },
+      },
     ],
 
     server: {
@@ -30,7 +83,8 @@ export default defineConfig(({ mode }) => {
       target: "esnext",
     },
     optimizeDeps: {
-      exclude: [],
+      //TODO: Add plugin for this and figure out exactly why this works
+      exclude: ["@wasmsharp/core"],
     },
     resolve: {
       //https://github.com/solidjs/solid-testing-library/issues/30
