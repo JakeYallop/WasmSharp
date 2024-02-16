e097a96bb42fddad4e457948ac3ebad5f9ce772a
JakeYallop <30874283+JakeYallop@users.noreply.github.com>
2024-02-16 14:38:43 +0000

    Minor styling tweaks

--- playground/src/index.css.ts
+++ playground/src/index.css.ts
@@ -12,6 +12,7 @@ globalStyle("#root", {
 globalStyle("html, body", {
   height: "100%",
   width: "100%",
+  fontSize: "110%",
   fontFamily: "Segoe UI,Helvetica Neue,sans-serif;",
   color: palette.text.body,
 });
